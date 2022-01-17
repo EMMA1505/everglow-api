35a60d6eacdbdbd88697dac4dee3a75d8e3b6c0c
EMMA1505 <emmanuelroamni@gmail.com>
2022-01-17 17:55:25 -0300

    add env variables

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,8 +2,11 @@ const mongodb = require('mongodb')
 
 const MongoClient = mongodb.MongoClient
 
+const dbUser = process.env.dbUser;
+const dbPass = process.env.dbPass;
+
 //const url = 'mongodb://localhost:27017'
-const url = 'mongodb+srv://Emma1505:mica26valen12@cluster0.m1ez6.mongodb.net/everglow_services?retryWrites=true&w=majority'
+const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.m1ez6.mongodb.net/everglow_services?retryWrites=true&w=majority`
 
 function getClient (callback){
     MongoClient.connect(url, (err, client) => {
